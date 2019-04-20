20957c503c0cf39605dbfa3a6aafea8223946b15
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-04-20 10:58:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,8 +22,8 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-def using_concat(array_one, arrray_two)
-  array_one.concat(array_two)
+def using_concat(array1, arrray2)
+  array1.concat(array2)
 end
 
 def using_insert(array, element)
